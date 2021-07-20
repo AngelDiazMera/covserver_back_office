924c688cbaefd0fff338691f5332b207eae0c1c5
Jorge Angel Diaz Mera <angel_diaz_mera@outlook.com>
2021-07-20 13:17:20 -0500

    navbar toggle bug fix

--- src/pages/user_page/components/_navbar/Navbar.tsx
+++ src/pages/user_page/components/_navbar/Navbar.tsx
@@ -1,70 +1,46 @@
-import { useEffect, useState } from 'react'
-import logo from '../img/img_cov.png'
-import {Collapse} from 'bootstrap';
+import { useEffect, useState } from "react";
+import logo from "../img/img_cov.png";
+import { Collapse } from "bootstrap";
 
 function Navbar() {
+  const [isNavActive, setIsNavActive] = useState(false);
 
   const myFunction = () => {
-    const e:any = document.querySelector(".sidebar"), o:any = document.querySelectorAll("#sidebarToggle, #sidebarToggleTop");
-    if (e) {
-      e.querySelector(".collapse");
-      var t = [].slice
-        .call(document.querySelectorAll(".sidebar .collapse"))
-        .map(function (e) {
-          return new Collapse(e, { toggle: !1 });
-        });
-      for (var l of o)
-        l.addEventListener("click", function () {
-          if (
-            (document.body.classList.toggle("sidebar-toggled"),
-            e.classList.toggle("toggled"),
-            e.classList.contains("toggled"))
-          )
-            for (var l of t) l.hide();
-        });
-      window.addEventListener("resize", function () {
-        if (
-          Math.max(
-            document.documentElement.clientWidth || 0,
-            window.innerWidth || 0
-          ) < 768
-        )
-          for (var e of t) e.hide();
-      });
-    } 
-  }; 
+    const bodyClasses = document.body.classList; // Classes of body DOM target
+    setIsNavActive(!isNavActive); // Toggles the isNavActive state
+    // If it is active, add to the body, the class 'sidebar-toggled'
+    // Else, remove it
+    if (isNavActive) {
+      // If body contains that class, return
+      if (bodyClasses.contains("sidebar-toggled")) return;
+      bodyClasses.add("sidebar-toggled");
+    } else bodyClasses.remove("sidebar-toggled");
+  };
 
-// Hook: When the user clicks the update button
-  useEffect(() => {  
-     
-      console.log("Hola");
-      myFunction();
-    
-    
-  }, []);
-
-    return (
-      <nav
-        className="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0"
-        style={{
-          background: "#303f9f"
-        }}
-      >
-        <div className="container-fluid d-flex flex-column p-0">
-        <div className="sidebar-brand-icon rotate-n-15" ></div>
+  return (
+    <nav
+      className={`navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0 ${
+        isNavActive ? "toggled" : ""
+      }`}
+      style={{
+        background: "#303f9f",
+      }}
+    >
+      <div className="container-fluid d-flex flex-column p-0">
+        <div className="sidebar-brand-icon rotate-n-15"></div>
         <div className="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0">
-        <img
-          src={logo} 
-          width="40px"
-          height="40px"
-          style={{
-            transform: "translate(0px)"
-          }}
-        />
+          <img
+            src={logo}
+            width="40px"
+            height="40px"
+            style={{
+              transform: "translate(0px)",
+            }}
+          />
           <div
             className="sidebar-brand-text mx-3"
             style={{
-              color: "white"
+              color: "white",
             }}
           >
             <span>Covserver</span>
@@ -73,36 +49,30 @@ function Navbar() {
         <hr className="sidebar-divider my-0" />
         <ul className="navbar-nav text-light" id="accordionSidebar">
           <li className="nav-item">
-            <a
-              className="nav-link"
-              href={'../dashboard'}
-            >
+            <a className="nav-link" href={"../dashboard"}>
               <i className="fas fa-tachometer-alt" />
               <span> Dashboard</span>
             </a>
-          </li> 
-          <li 
-          className="nav-item">
-              <a 
-              className="nav-link" href="#">
-                  <i className="fas fa-user">
-                      </i><span> Mi cuenta</span></a>
           </li>
-          <li 
-          className="nav-item">
-              <a 
-              className="nav-link" href={'../employees'}>
-                  <i className="fas fa-table">
-                      </i><span> Empleados</span></a>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              <i className="fas fa-user"></i>
+              <span> Mi cuenta</span>
+            </a>
           </li>
-          <li 
-          className="nav-item">
-              <a 
-              className="nav-link" href="#">
-                  <i className="fas fa-user-circle">
-                      </i><span> Cerrar sesión</span></a>
+          <li className="nav-item">
+            <a className="nav-link" href={"../employees"}>
+              <i className="fas fa-table"></i>
+              <span> Empleados</span>
+            </a>
           </li>
-        </ul>   
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              <i className="fas fa-user-circle"></i>
+              <span> Cerrar sesión</span>
+            </a>
+          </li>
+        </ul>
 
         <div className="text-center d-none d-md-inline">
           <button
@@ -113,10 +83,8 @@ function Navbar() {
           />
         </div>
       </div>
- 
-      </nav>
-    );
-  
+    </nav>
+  );
 }
 
 export default Navbar;
