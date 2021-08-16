5557f69141ae83c5aaba3b1e9687faf9c9ac8d35
Jorge Angel Diaz Mera <angel_diaz_mera@outlook.com>
2021-08-16 12:32:05 -0500

    process api

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 // Configure the base URL from the api
 export default axios.create({
-    baseURL: 'https://covserverbackend.herokuapp.com/'
+    baseURL: process.env.API || 'https://covserverbackend.herokuapp.com/'
     // baseURL: 'https://z0be02405-z93995d29-gtw.qovery.io/'
 });
