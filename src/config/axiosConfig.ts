f7a729fb561369fbe7cd37d32af372f13a122b8f
Jorge Angel Diaz Mera <angel_diaz_mera@outlook.com>
2021-08-16 12:25:53 -0500

    heroku server

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -2,4 +2,5 @@ import axios from 'axios'
 // Configure the base URL from the api
 export default axios.create({
     baseURL: 'https://covserverbackend.herokuapp.com/'
+    // baseURL: 'https://z0be02405-z93995d29-gtw.qovery.io/'
 });
